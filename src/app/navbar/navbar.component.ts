8b7bebffe8414a8d9d0f360035d4a2d08faaf195
Liza Kovacs <kovacs.liza0@gmail.com>
2024-03-05 12:25:07 +0100

    navbar responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  private isDropdownOpen = false;
 
+  constructor(private renderer: Renderer2, private router: Router) {}
+
+  ngOnInit() {
+    const btnNavEl = document.querySelector(
+      '.btn-mobile-nav'
+    ) as HTMLButtonElement;
+    const headerEl = document.querySelector('.header') as HTMLElement;
+
+    // Initialize with the dropdown closed
+    this.renderer.removeClass(headerEl, 'nav-open');
+
+    btnNavEl.addEventListener('click', () => {
+      this.isDropdownOpen = !this.isDropdownOpen;
+      if (this.isDropdownOpen) {
+        headerEl.classList.add('nav-open');
+      } else {
+        headerEl.classList.remove('nav-open');
+      }
+    });
+
+    // Close dropdown and navigate when a link is clicked
+    const dropdownLinks = document.querySelectorAll('.main-nav-list a');
+    dropdownLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        this.isDropdownOpen = false;
+        headerEl.classList.remove('nav-open');
+        // Get the routerLink attribute from the clicked link
+        const routerLink = link.getAttribute('routerLink');
+        // Navigate to the specified route
+        if (routerLink) {
+          this.router.navigateByUrl(routerLink);
+        }
+      });
+    });
+  }
 }
