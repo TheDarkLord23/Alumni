99610de662ec8f56c0f7e78a6354f823aaac8a10
DacianSB <sasdacian@gmx.at>
2024-03-07 10:33:44 +0100

    changed

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
-  styleUrl: './stories.component.scss'
+  styleUrl: './stories.component.scss',
 })
 export class StoriesComponent {
+  windowScrolled = false;
 
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset !== 0;
+    });
+  }
 }
